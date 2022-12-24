aa2b3e3a27f61fb059b557a02fcfd2eb598e679c
Tri Vu <vutriminh@gmail.com>
2022-12-24 03:41:28 -0500

    added bootstrap to index.js and styled the series into cards

--- src/index.js
+++ src/index.js
@@ -10,6 +10,8 @@ import {Provider} from 'react-redux';
 import {createStore} from 'redux'
 import rootReducer from './reducers/reducer';
 
+import 'bootstrap/dist/css/bootstrap.min.css';
+
 const store = createStore(
   rootReducer,
   window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__() 
