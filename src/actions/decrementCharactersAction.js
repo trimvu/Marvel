12115869264f0155169002da021f8ea320f2c1fd
Tri Vu <vutriminh@gmail.com>
2022-12-25 23:06:15 -0500

    MERRY CHRISTMAS! added decrementCharactersAction.js and incrementCharactersActions.js

---
+++ src/actions/decrementCharactersAction.js
@@ -0,0 +1,11 @@
+
+const decrementCharacterssAction = (num) => {
+
+    return {
+        type: "DECREMENT_CHARACTERS_ITEMS",
+        data: num
+    }
+
+}
+
+export default decrementCharacterssAction;
