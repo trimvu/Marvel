9e5772ca8cdfb2f205efbd1a391e15c40d38a60b
Tri Vu <vutriminh@gmail.com>
2022-12-27 23:26:06 -0500

    populated info on the Comic.js page. able to link from Characters.js page to Comic.js page

--- src/components/series/Series.js
+++ src/components/series/Series.js
@@ -1,10 +1,34 @@
 import React, { useEffect, useState } from 'react'
-import { useParams, Link } from 'react-router-dom'
+import { useParams } from 'react-router-dom'
+import { useDispatch } from 'react-redux'
+import '../style/Characters.css'
+
+import CharactersCards from './seriesCards/CharactersCards'
+import ComicCards from './seriesCards/ComicCards'
+import EventsCards from './seriesCards/EventsCards'
+import StoriesCards from './seriesCards/StoriesCards'
 
 const Series = () => {
 
+  const API_KEY = process.env.REACT_APP_API_KEY
 
-    let {series} = useParams()
+  let {series} = useParams()
+
+  const seriesDetail = async () => {
+
+    const data = await fetch(`https://gateway.marvel.com:443/v1/public/series?title=${series}&apikey=${API_KEY}`)
+
+    const details = await data.json()
+
+    console.log("series details:", details)
+  }
+
+  useEffect(()=> {
+
+    seriesDetail();
+
+  }, [])
+  
   return (
     <>
         <h1>Series: {series}</h1>
