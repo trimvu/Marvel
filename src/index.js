8a81fe58d841e99cb0a33f6759dfde4113e9fe87
Tri Vu <vutriminh@gmail.com>
2023-01-21 00:52:23 -0500

    installed node-sass and implemented new scss.

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-// import './index.scss'
+import './index.scss'
 import App from './App';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import BaseLayout from './components/layout/BaseLayout';
